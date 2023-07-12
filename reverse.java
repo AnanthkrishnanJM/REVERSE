eac4f14e403b82c910c310a5dc9dff682fa4ae70
JAYAPRASHANT <jayaprashanth.20eee@gmail.com>
2023-07-12 16:33:51 +0530

    problem

---
+++ reverse.java
@@ -0,0 +1,18 @@
+package jmak;
+
+import java.util.Scanner;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		
+		int a[]= {1,2,3,2,1};
+	
+		for(int i=4;i>=0;i--){
+			System.out.println(a[i]);
+			
+		}
+
+	}
+
+}
